21cae743fbf12045b50441c2c3f80992e35449b6
MOH Lamine <mmmbbbhhh17@gmail.com>
2021-10-27 19:57:42 +0100

    fix the doit problem

--- src/App.java
+++ src/App.java
@@ -4,6 +4,12 @@ public class App {
 
         new MyFrame();
 
+        // String test = "1...2.3.3.44";
+        // System.out.println();
+        // System.out.println(">>> " + test);
+        // System.out.println(">>> " + TP01_EX01.splitToWords(test));
+        // System.out.println();
+
     }
 
 }
--- src/TP01_EX01.java
+++ src/TP01_EX01.java
@@ -5,9 +5,11 @@ public class TP01_EX01 {
     }
 
     public static String splitToWords(String code) {
+
         code = code.replace(" ", "#");
         code = code.replace("\n", "#");
         code = code.replace("(", "#(#");
+        code = code.replace(")", "#)#");
         code = code.replace("+", "#+#");
         code = code.replace("-", "#-#");
         code = code.replace("*", "#*#");
@@ -29,12 +31,30 @@ public class TP01_EX01 {
         // //
         char[] codeList = code.toCharArray();
         code = "";
+        boolean numberBeforeDoit = true;
+
         for (int i = 0; i < codeList.length; i++) {
-            code += codeList[i];
+            int j = i;
             if ((codeList[i] == '+' || codeList[i] == '-') && (i < 2 || codeList[i - 2] == '+' || codeList[i - 2] == '-'
                     || codeList[i - 2] == '*' || codeList[i - 2] == '/' || codeList[i - 2] == '(')) {
-                i++;
+                // get the signs //
+                j++;
+            } else if (numberBeforeDoit == false && !"0123456789".contains("" + codeList[i])) {
+                if (i < codeList.length - 1) {
+                    if (codeList[i] != '.') {
+                        numberBeforeDoit = true;
+                    }
+
+                    if (codeList[i] != '#') {
+                        code += "#";
+                    }
+                }
+            } else if (codeList[i] == '.') {
+                numberBeforeDoit = false;
             }
+
+            code += codeList[i];
+            i = j;
         }
         // //
 
