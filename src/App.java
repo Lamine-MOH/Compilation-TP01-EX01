4e0d874f1468568dd379b26ef4db390a3ed402bc
MOH Lamine <mmmbbbhhh17@gmail.com>
2021-11-15 12:39:55 +0100

    fix the dot connecting problem

--- src/App.java
+++ src/App.java
@@ -1,36 +1,34 @@
 
-public class App {
-    public static void main(String[] args) throws Exception {
+// public class App {
+//     public static void main(String[] args) throws Exception {
 
-        new MyFrame();
+//         new MyFrame();
 
-    }
-
-}
-
-// ------------------------------ with javaFX ------------------------------
-// import java.io.IOException;
-
-// import javafx.application.Application;
-// import javafx.fxml.FXMLLoader;
-// import javafx.scene.Scene;
-// import javafx.scene.layout.Pane;
-// import javafx.stage.Stage;
-
-
-
-// public class App extends Application {
-//     public static void main(String[] args) {
-//         launch(args);
 //     }
 
-//     @Override
-//     public void start(Stage primaryStage) throws IOException {
-//         Pane mainPane = (Pane) FXMLLoader.load(App.class.getResource("FXML/frame.fxml"));
-
-//         primaryStage.setScene(new Scene(mainPane));
-//         primaryStage.show();
-//     }
 // }
 
+//------------------------------with javaFX------------------------------
 
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+// import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+public class App extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        Parent mainPane = FXMLLoader.load(App.class.getResource("frame.fxml"));
+
+        primaryStage.setScene(new Scene(mainPane));
+        primaryStage.show();
+    }
+}
--- src/TP01_EX01.java
+++ src/TP01_EX01.java
@@ -4,6 +4,21 @@ public class TP01_EX01 {
     TP01_EX01() {
     }
 
+    public static String removeExtraSharp(String code){
+        boolean test = true;
+        while (test == true) {
+            test = false;
+            int oldLength = code.length();
+
+            code = code.replace("##", "#");
+            if (code.length() < oldLength) {
+                test = true;
+            }
+        }
+
+        return code;
+    }
+
     public static String splitToWords(String code) {
 
         code = code.replace(" ", "#");
@@ -18,58 +33,46 @@ public class TP01_EX01 {
         code = code + "#";
 
         // remove extra # //
-        boolean test = true;
-        while (test == true) {
-            test = false;
-            int oldLength = code.length();
-
-            code = code.replace("##", "#");
-            if (code.length() < oldLength) {
-                test = true;
-            }
-        }
+        code = removeExtraSharp(code);
         // //
 
         // //
         char[] codeList = code.toCharArray();
         code = "";
-        boolean numberBeforeDoit = true;
+        // boolean numberBeforeDoit = true;
 
+        int startIndex = 0;
         if (codeList[0] == '#') {
-            codeList[0] = ' ';
+            startIndex = 1;
         }
 
-        for (int i = 0; i < codeList.length; i++) {
+        for (int i = startIndex; i < codeList.length; i++) {
             int j = i;
             if ((codeList[i] == '+' || codeList[i] == '-')
                     && (i < 2 || codeList[i - 2] == '+' || codeList[i - 2] == '-' || codeList[i - 2] == '*'
                             || codeList[i - 2] == '/' || codeList[i - 2] == '(')
-                    && (i + 1 < codeList.length && "0123456789".contains("" + codeList[i + 2]))) {
+                    && (i + 1 < codeList.length && "0123456789#".contains("" + codeList[i + 2]))) {
                 // get the signs //
                 j++;
 
-            } else if (i>0&&codeList[i] == '.' && codeList[i - 1] == '.') {
+            } else if (i > 0 && codeList[i] == '.' && !("0123456789".contains("" + codeList[i - 1]))) {
                 code += "#";
             }
-            // else if (numberBeforeDoit == false && !"0123456789".contains("" +
-            // codeList[i])) {
-            // if (i < codeList.length - 1) {
-            // if (codeList[i] != '.') {
-            // numberBeforeDoit = true;
-            // }
-
-            // if (codeList[i] != '#') {
-            // code += "#";
-            // }
-            // }
-            // } else if (codeList[i] == '.') {
-            // numberBeforeDoit = false;
-            // }
 
             code += codeList[i];
+
+            if (i < codeList.length && codeList[i] == '.' && !("0123456789#".contains("" + codeList[i + 1]))) {
+                code += "#";
+            }
+
             i = j;
         }
         // //
+
+        // remove extra # //
+        code = removeExtraSharp(code);
+        // //
+
 
         return code;
     }
